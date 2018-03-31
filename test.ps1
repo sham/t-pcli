1360838804c7cfa0c5bd174f6e6dea9655a75c09
Shyam <shyam@virtualsa.com>
2018-03-31 14:36:34 +0100

    test user

--- test.ps1
+++ test.ps1
@@ -1,3 +1,3 @@
 Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false
-get-vc -server 192.168.222.131 -User Administrator@vsphere.local -Password letme1n!!!!!!@
+get-vc -server 192.168.222.131 -User test@vsphere.local -Password Letme1nn@w
 get-vmhost
